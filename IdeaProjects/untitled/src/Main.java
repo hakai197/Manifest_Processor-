c9a719baa654a070046d4ad2e71767549d26a689
Nicholas Mathias <hakai197@gmail.com>
2025-01-29 11:18:05 -0500

    Manifest Class Created

--- IdeaProjects/untitled/src/Main.java
+++
@@ -1,7 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        String s1 = "ALL MEN ARE CREATED EQUAL";
-        String s2 = s1.substring(16, 19);
-
-        System.out.println(s2);}
